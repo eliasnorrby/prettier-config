eb62604b5e20a85b46a81daac229a844cadb4d59
Elias Norrby <elias.norrby@gmail.com>
2019-11-14 07:33:22 +0100

    feat: add --no-install option
    Adds parsing of command line options using yargs. The --no-install flag
    can be specified to run the setup without leaving any footprint leading
    back to this specific package. That is, this package will not be
    installed as a dependency. A project configured this way will not
    benefit from updates to the config, but will instead get a hard-coded
    set of rules to start with. This is useful for external projects where I
    want the benefit of an automatic configuration but don't want any ties
    to my personal package.
    
    Revamps test structure to allow for the different paths the script now
    can take.
    
    The ignore-file is read from disk instead of being duplicated in the
    setup script. DRY!
    
    Closes: #6

--- setup.test.sh
+++ setup.test.sh
@@ -3,45 +3,91 @@
 
 set -exo pipefail;
 
+PI_LINES=$(wc -l .prettierignore | cut -d " " -f1)
+EXPECTED_LINES=$((PI_LINES+1))
+
 ORIG_DIR=$(pwd)
 function finish {
+  if [ ! $? -eq 0 ] ; then
+    echo "There are failing tests"
+  else
+    echo "All tests passed!"
+  fi
   cd "$ORIG_DIR"
 }
 trap finish EXIT
 
-cd "$(mktemp -d)"
-npm init -y
-npx $ORIG_DIR
-ls -a
+function setup {
+  local FLAG=$1
+  cd "$(mktemp -d)"
+  npm init -y
+  npx $ORIG_DIR $FLAG
+  ls -a
+}
 
-echo "'.prettierignore' should exist"
-[ -e ".prettierignore" ]
+function common_test {
+  echo "'.prettierignore' should exist"
+  [ -e ".prettierignore" ]
 
-echo "'.prettierignore' should have 8 lines"
-[ "$(wc -l .prettierignore | cut -d ' ' -f1)" -eq 8 ]
+  echo "'.prettierignore' should have $EXPECTED_LINES lines"
+  [ "$(wc -l .prettierignore | cut -d ' ' -f1)" -eq $EXPECTED_LINES ]
 
-echo "'prettier.config.js' should exist"
-[ -e "prettier.config.js" ]
+  echo "'prettier.config.js' should exist"
+  [ -e "prettier.config.js" ]
 
-echo "'@eliasnorrby/prettier-config' should be installed"
-[ -d "node_modules/@eliasnorrby/prettier-config" ]
+  echo "'prettier' should be installed"
+  [ -d "node_modules/prettier" ]
 
-echo "'prettier' should be installed"
-[ -d "node_modules/prettier" ]
+  echo "format script should exist"
+  npm run format
 
-echo "format script should exist"
-npm run format
+  echo "checkFormat script should exist"
+  npm run check-format
+}
 
-echo "checkFormat script should exist"
-npm run check-format
+function ignore_test {
 
-cat <<EOF > .prettierignore
+  local FLAG=$1
+  cat <<EOF > .prettierignore
 ignored-file
 node_modules/
 EOF
 
-npx $ORIG_DIR
+  npx $ORIG_DIR $FLAG
 
-echo "should not overwrite existing '.prettierignore'"
-echo "'.prettierignore' should have 11 lines"
-[ "$(wc -l .prettierignore | cut -d ' ' -f1)" -eq 11 ]
+  echo "should not overwrite existing '.prettierignore'"
+  echo "'.prettierignore' should have > $EXPECTED_LINES lines"
+  [ "$(wc -l .prettierignore | cut -d ' ' -f1)" -gt $EXPECTED_LINES ]
+}
+
+function install_test {
+  setup
+
+  common_test
+
+  echo "'@eliasnorrby/prettier-config' should be installed"
+  [ -d "node_modules/@eliasnorrby/prettier-config" ]
+
+  ignore_test
+}
+
+function no_install_test {
+  setup "--no-install"
+
+  common_test
+
+  echo "'@eliasnorrby/prettier-config' should not be installed"
+  [ ! -d "node_modules/@eliasnorrby/prettier-config" ]
+
+  ignore_test "--no-install"
+}
+
+function help_test {
+  npx $ORIG_DIR --help | grep "Usage"
+}
+
+install_test
+
+no_install_test
+
+help_test
